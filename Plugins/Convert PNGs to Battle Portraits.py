79df8cb6c738df7b2e1966ca95b1f36e2ca91c99
mawwk <>
2021-05-03 23:05:05 -0500

    Convert PNGs to Battle Portraits - fix error on prompt exit

--- Plugins/Convert PNGs to Battle Portraits.py
+++ Plugins/Convert PNGs to Battle Portraits.py
@@ -1,5 +1,5 @@
 __author__ = "mawwwk"
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 from BrawlCrate.API import *
 from BrawlLib.SSBB import FileFilters
@@ -40,33 +40,35 @@ else:
 
 # Prompt for PNG file paths
 images = BrawlAPI.OpenMultiFileDialog("Select battle portrait images", FileFilters.TEX0)
-OUTPUT_DIR = images[0].rsplit("\\",1)[0]
 
-# Take user input to determine starting InfFace file name
-INITIAL_BP_ID = getStartingPortraitID()
+if images:
+	OUTPUT_DIR = images[0].rsplit("\\",1)[0]
 
-# Reference https://soopercool101.github.io/BrawlCrate/class_brawl_crate_1_1_external_interfacing_1_1_color_smash.html#aa4fadb82e34ae150d1e94ab101399a62
-# Iterate through each image opened and export a brres containing the texture in CI8
-exportedImageCount = 0
-for image in images:
-	outputPath = OUTPUT_DIR + "\\InfFace" + addLeadingZeros(INITIAL_BP_ID + exportedImageCount) + ".brres"
-	
-	BrawlAPI.New[BRRESNode]()
-	
-	dlg = TextureConverterDialog()
-	dlg.ImageSource = image
-	dlg.Automatic = True
-	dlg.StartingFormat = WiiPixelFormat.CI8
-	
-	if (dlg.ShowDialog(MainForm.Instance, BrawlAPI.RootNode) == dlg.DialogResult.OK):
-		BrawlAPI.SaveFileAs(outputPath)
-		exportedImageCount += 1
+	# Take user input to determine starting InfFace file name
+	INITIAL_BP_ID = getStartingPortraitID()
 
-# After exporting, open previously opened file, if it exists
-if CURRENT_OPEN_FILE:
-	BrawlAPI.OpenFile(CURRENT_OPEN_FILE)
-# Otherwise, leave the last-exported BP open
-else:
-	BrawlAPI.OpenFile(outputPath)
+	# Reference https://soopercool101.github.io/BrawlCrate/class_brawl_crate_1_1_external_interfacing_1_1_color_smash.html#aa4fadb82e34ae150d1e94ab101399a62
+	# Iterate through each image opened and export a brres containing the texture in CI8
+	exportedImageCount = 0
+	for image in images:
+		outputPath = OUTPUT_DIR + "\\InfFace" + addLeadingZeros(INITIAL_BP_ID + exportedImageCount) + ".brres"
+		
+		BrawlAPI.New[BRRESNode]()
+		
+		dlg = TextureConverterDialog()
+		dlg.ImageSource = image
+		dlg.Automatic = True
+		dlg.StartingFormat = WiiPixelFormat.CI8
+		
+		if (dlg.ShowDialog(MainForm.Instance, BrawlAPI.RootNode) == dlg.DialogResult.OK):
+			BrawlAPI.SaveFileAs(outputPath)
+			exportedImageCount += 1
 
-BrawlAPI.ShowMessage(str(exportedImageCount) + " BRRES files successfully exported to\n" + OUTPUT_DIR, "Success!")
+	# After exporting, open previously opened file, if it exists
+	if CURRENT_OPEN_FILE:
+		BrawlAPI.OpenFile(CURRENT_OPEN_FILE)
+	# Otherwise, leave the last-exported BP open
+	else:
+		BrawlAPI.OpenFile(outputPath)
+
+	BrawlAPI.ShowMessage(str(exportedImageCount) + " BRRES files successfully exported to\n" + OUTPUT_DIR, "Success!")
