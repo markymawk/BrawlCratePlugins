1e300c822ab66153306925ef48d733f9c26190bd
mawwk <>
2021-08-10 18:32:47 -0500

    Colorsmash-Safe Texture Sort - can now be run from parent BRRES, though texture nodes must still be visible

--- Loaders/Colorsmash-Safe Texture Sort.py
+++ Loaders/Colorsmash-Safe Texture Sort.py
@@ -11,13 +11,28 @@ from mawwwkLib import *
 SCRIPT_NAME = "Color Smash-safe Texture Sort"
 moveCount = 0
 
+TEXTURE_GROUP_ERROR_TEXT = "Error: expand the Textures(NW4R) group within the BRRES so that the green texture nodes are visible, then try the plug-in again."
+
 ## Start enable check functions
+
 # Check to ensure that the BRESGroup is a Texture group that contains more than 1 TEX0
 # Wrapper: BRESGroupWrapper
 def EnableCheckBRESGroup(sender, event_args):
 	group = BrawlAPI.SelectedNode
 	sender.Enabled = (group is not None and group.HasChildren and len(group.Children) > 1)
 
+# Check to ensure that the BRES contains a Texture group that contains more than 1 TEX0
+# Wrapper: BRESWrapper
+def EnableCheckBRES(sender, event_args):
+	group = BrawlAPI.SelectedNode
+	
+	if group is not None:
+		textureGroup = getChildFromName(group, "Texture")
+	else:
+		textureGroup = 0
+		
+	sender.Enabled = (group is not None and textureGroup and textureGroup.HasChildren and len(textureGroup.Children) > 1)
+	
 ## End enable check functions
 ## Start helper functions
 
@@ -35,7 +50,7 @@ def moveUpWrapper(wrapper):
 	moveCount += 1
 	wrapper.MoveUp(False)
 
-# Function to sort textures given the list of wrappers
+# Helper function to sort textures given the list of wrappers
 def sortTextures(textureWrappers, AUTO_SAVE):
 	sharesDataMoveUpCount = 0
 	tex0MovedCount = 1
@@ -45,6 +60,7 @@ def sortTextures(textureWrappers, AUTO_SAVE):
 	
 	# Iterate through textures
 	for i in range(1, len(textureWrappers), 1):
+	
 		# Auto-save if enabled, and only if not amid a colorsmash group
 		if AUTO_SAVE and not inSharesDataGroup and tex0MovedCount >= 13:
 			BrawlAPI.SaveFile()
@@ -60,6 +76,7 @@ def sortTextures(textureWrappers, AUTO_SAVE):
 		# Sort behavior #1: If current tex0 SharesData is False, and not in a colorsmash group, use MoveUp() freely as needed
 		if not currentWrapper.Resource.SharesData and not inSharesDataGroup:
 			while doLoop:
+			
 				# Compare current tex0 name with prevSibling tex0 name
 				prevWrapperNode = wrapperPrevSiblingNode(textureWrappers, currentWrapper)
 				if prevWrapperNode and currentWrapper.Resource.Name.lower() < prevWrapperNode.Name.lower():
@@ -68,14 +85,17 @@ def sortTextures(textureWrappers, AUTO_SAVE):
 					
 					# If the current tex0 was moved into the middle of a colorsmash group (previous.SharesData == True), push it back to preserve the CS order
 					previous = wrapperPrevSiblingNode(textureWrappers, currentWrapper)
+					
 					while previous and previous.SharesData:
 						moveUpWrapper(currentWrapper)
 						previous = wrapperPrevSiblingNode(textureWrappers, currentWrapper)
-				# If current node is sorted correctly, exit the loop
+				
+				# Once the current node is sorted correctly, exit the loop for this texture
 				else:
 					doLoop = False
 		
-		# Sort behavior #2: If inside a colorsmash group, MoveUp() the exact amount as the previously sorted tex0
+		# Sort behavior #2: If the current texture is inside a colorsmash group (i.e. previous node was SharesData == True),
+		# then MoveUp() the exact amount as the previously sorted tex0
 		elif inSharesDataGroup:
 			if sharesDataMoveUpCount:
 				tex0MovedCount += 1
@@ -100,16 +120,17 @@ def sortTextures(textureWrappers, AUTO_SAVE):
 				if prevWrapperNode and currentWrapper.Resource.Name.lower() < prevWrapperNode.Name.lower():
 					tex0MovedCount += 1
 					moveUpWrapper(currentWrapper)
-					sharesDataMoveUpCount += 1
+					sharesDataMoveUpCount += 1	# Store how many MoveUp() runs to do for the rest of the group
 					
 					# If the current tex0 was moved into the middle of a separate colorsmash group, push it back to preserve the other group's order
 					previous = wrapperPrevSiblingNode(textureWrappers, currentWrapper)
+					
 					while previous and previous.SharesData:
 						moveUpWrapper(currentWrapper)
-						sharesDataMoveUpCount += 1
 						previous = wrapperPrevSiblingNode(textureWrappers, currentWrapper)
+						sharesDataMoveUpCount += 1
 				
-				# If current node is sorted correctly, exit the loop		
+				# Once the current current node is sorted correctly, exit the loop for this texture
 				else:
 					doLoop = False
 	
@@ -118,13 +139,26 @@ def sortTextures(textureWrappers, AUTO_SAVE):
 ## End helper functions
 ## Start loader functions
 
-def colorsmash_safe_sort(sender, event_args):
-	# Begin by checking that the Textures group is expanded and that TEX0 nodes are visible. The script will crash otherwise
-	textureWrappers = BrawlAPI.SelectedNodeWrapper.Nodes
+def colorsmash_safe_sort_bresgroup(sender, event_args):
+	main(BrawlAPI.SelectedNodeWrapper)
+
+def colorsmash_safe_sort_bres(sender, event_args):
+
+	if str(BrawlAPI.SelectedNodeWrapper.Nodes[0]) == "TreeNode: ":
+		BrawlAPI.ShowError(TEXTURE_GROUP_ERROR_TEXT, "Error")
+		return
+	else:	
+		main(getChildWrapperFromName(BrawlAPI.SelectedNodeWrapper, "Textures"))
+	
+## End loader functions
+## Start main function
+
+def main(parentGroupWrapper):
+	TEXTURE_WRAPPERS = parentGroupWrapper.Nodes
 	
 	# If type is null, don't run the script
-	if str(textureWrappers[0]) == "TreeNode: ":
-		BrawlAPI.ShowError("Error: expand the Textures(NW4R) group within the BRRES, then try the plug-in again.","Error")
+	if str(TEXTURE_WRAPPERS[0]) == "TreeNode: ":
+		BrawlAPI.ShowError(TEXTURE_GROUP_ERROR_TEXT, "Error")
 		return
 	
 	# User prompt to run
@@ -139,15 +173,19 @@ def colorsmash_safe_sort(sender, event_args):
 	AUTO_SAVE = BrawlAPI.ShowYesNoPrompt(message, SCRIPT_NAME)
 	
 	# If 2 or more textures, start the sort. (This is in EnableCheck already, but verify the wrapper count to be double-sure)
-	if len(textureWrappers) >= 2:
-		sortTextures(textureWrappers, AUTO_SAVE)
+	if len(TEXTURE_WRAPPERS) >= 2:
+		sortTextures(TEXTURE_WRAPPERS, AUTO_SAVE)
 		
 		if moveCount == 0:
 			BrawlAPI.ShowMessage("Sort complete: no resorting done.", SCRIPT_NAME)
 		else:
 			BrawlAPI.ShowMessage("Sort complete! (" + str(moveCount) + " total moves performed)", "Success!")
-	
-## End loader functions
+
+## End main function
 ## Start context menu add
 
-BrawlAPI.AddContextMenuItem(BRESGroupWrapper, "", "Sort textures in order while preserving color smash sets", EnableCheckBRESGroup, ToolStripMenuItem("Sort Textures (Color Smash-safe)", None, colorsmash_safe_sort))
+# From Textures group
+BrawlAPI.AddContextMenuItem(BRESGroupWrapper, "", "Sort textures in order while preserving color smash sets", EnableCheckBRESGroup, ToolStripMenuItem("Sort Textures (Color Smash-safe)", None, colorsmash_safe_sort_bresgroup))
+
+# From parent BRRES
+BrawlAPI.AddContextMenuItem(BRESWrapper, "", "Sort textures in order while preserving color smash sets", EnableCheckBRES, ToolStripMenuItem("Sort Textures (Color Smash-safe)", None, colorsmash_safe_sort_bres))
