9e3d97b5ea38626a5312697e6c4d6da741002178
mawwwk <>
2023-05-15 04:47:10 -0500

    Import PLT0 - new loader to import external PLT0 files

---
+++ Loaders/Import PLT0.py
@@ -0,0 +1,39 @@
+__author__ = "mawwwk"
+__version__ = "1.0"
+
+from BrawlCrate.API import *
+from BrawlCrate.NodeWrappers import *
+from BrawlLib.SSBB.ResourceNodes import *
+from System.Windows.Forms import ToolStripMenuItem
+from mawwwkLib import *
+
+## Start enable check function
+# Check that group has Palettes in name
+# Wrapper: BRESGroupWrapper
+def EnableCheckGroupWrapper(sender, event_args):
+	node = BrawlAPI.SelectedNode
+	sender.Enabled = (node is not None and "Palettes(" in node.Name and node.HasChildren)
+
+## End enable check function
+## Start loader function
+
+def import_plt0(sender, event_args):
+	plt0Files = BrawlAPI.OpenMultiFileDialog("Select PLT0 files", "Palettes (*.plt0)|*.plt0")
+	if not plt0Files:
+		return
+	
+	basePalette = BrawlAPI.SelectedNodeWrapper.Nodes[0]
+	
+	# Loop through PLT0 files to add
+	for file in plt0Files:
+		# Get file name to rename palette
+		fileName = file.split("\\")[-1][:-5]
+		# Duplicate base palette, rename, and replace
+		newPLT0 = basePalette.Duplicate()
+		newPLT0.Name = fileName
+		newPLT0.Replace(file)
+
+## End loader function
+## Start context menu add
+
+BrawlAPI.AddContextMenuItem(BRESGroupWrapper, "", "Import palettes from external PLT0 files", EnableCheckGroupWrapper, ToolStripMenuItem("Import PLT0 files...", None, import_plt0))
