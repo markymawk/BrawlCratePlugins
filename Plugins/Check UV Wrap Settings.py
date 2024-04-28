11cdb0073699b4d0f3295cd22b4005c5dff42508
mawwwk <>
2024-04-28 02:48:02 -0500

    Check UV Wrap Settings - Check height and width separately. Cap max list items at 15

--- Plugins/Check UV Wrap Settings.py
+++ Plugins/Check UV Wrap Settings.py
@@ -3,7 +3,6 @@ __version__ = "1.0"
 
 from BrawlCrate.API import *
 from BrawlLib.SSBB.ResourceNodes import *
-
 from mawwwkLib import *
 
 # Verify that any textures whose resolution isn't
@@ -18,46 +17,63 @@ def main():
 	
 	if not BrawlAPI.ShowOKCancelPrompt(startMsg, SCRIPT_NAME):
 		return
-	textureNamesToCheck = []
+	textureNamesToCheck_Width = []
+	textureNamesToCheck_Height = []
 	
 	# Get list of textures to check
 	for tex0 in BrawlAPI.NodeListOfType[TEX0Node]():
-		if tex0.Width not in VALID_TEXTURE_RES \
-		or tex0.Height not in VALID_TEXTURE_RES:
-			textureNamesToCheck.append(tex0.Name)
+		if tex0.Width not in VALID_TEXTURE_RES:
+			textureNamesToCheck_Width.append(tex0.Name)
+		if tex0.Height not in VALID_TEXTURE_RES:
+			textureNamesToCheck_Height.append(tex0.Name)
 	
 	# If all textures are powers of 2, quit
-	if not len(textureNamesToCheck):
+	if not len(textureNamesToCheck_Width + textureNamesToCheck_Height):
 		BrawlAPI.ShowMessage("All textures in this file use powers of 2.", SCRIPT_NAME)
 		return
 	
 	# Get list of materials used by these textures
-	matRefsToCheck = []
-	badMaterialWraps = []
+	badMaterialWraps_U = []
+	badMaterialWraps_V = []
+	
 	for mat in BrawlAPI.NodeListOfType[MDL0MaterialNode]():
 		# Skip mats with no tex refs
 		if not mat.HasChildren:
 			continue
 		
 		for matRef in mat.Children:
-			for badTex in textureNamesToCheck:
+			# Check Height / UWrapMode
+			for badTex in textureNamesToCheck_Width:
 				
 				# If texture found and not set to Clamp, add to list
-				if badTex == matRef.Name and (matRef.UWrapMode != MatWrapMode.Clamp or matRef.VWrapMode != MatWrapMode.Clamp):
-					badMaterialWraps.append(matRef)
+				if badTex == matRef.Name and matRef.UWrapMode != MatWrapMode.Clamp:
+					badMaterialWraps_U.append(matRef)
+				
+			# Check Width / VWrapMode
+			for badTex in textureNamesToCheck_Height:
+				# If texture found and not set to Clamp, add to list
+				if badTex == matRef.Name and matRef.VWrapMode != MatWrapMode.Clamp:
+					badMaterialWraps_V.append(matRef)
 	
 	# If all materials are using Clamp as needed, quit
-	if not len(badMaterialWraps):
-		BrawlAPI.ShowMessage("No incorrect wrap mode settings found for the following textures:\n\n" + listToString(textureNamesToCheck), SCRIPT_NAME)
+	maxListItems = 15
+	if not len(badMaterialWraps_U + badMaterialWraps_V):
+		msg = "No incorrect wrap mode settings found for the following textures:\n\n" + \
+		listToStringNoDuplicates(textureNamesToCheck_Height + textureNamesToCheck_Width, maxListItems)
+		
+		BrawlAPI.ShowMessage(msg, SCRIPT_NAME)
 		return
 	
 	# If materials use incorrect wrap, prompt to auto set them
-	msg = "The following material refs are not set to Clamp UVs:\n\n"
-	for matRef in badMaterialWraps:
+	formattedBadMats = []
+	for matRef in badMaterialWraps_U + badMaterialWraps_V:
 		mat = matRef.Parent
 		mdl0 = mat.Parent.Parent
 		brres = mdl0.Parent.Parent
-		msg += brres.Name + "/" + mdl0.Name + "/" + mat.Name + ": " + matRef.Name + "\n"
+		formattedBadMats.append(brres.Name + "/" + mdl0.Name + "/" + mat.Name + ": " + matRef.Name)
+	
+	msg = "The following material refs are not set to Clamp UVs:\n\n"
+	msg += listToStringNoDuplicates(formattedBadMats, maxListItems)
 	msg += "\nSet these to Clamp now?"
 	
 	# If not auto-setting any materials, quit
@@ -65,11 +81,12 @@ def main():
 		return
 	
 	# Set to Clamp
-	for matRef in badMaterialWraps:
+	for matRef in badMaterialWraps_U:
 		matRef.UWrapMode = MatWrapMode.Clamp
+	for matRef in badMaterialWraps_V:
 		matRef.VWrapMode = MatWrapMode.Clamp
 	
-	badMatCount = str(len(badMaterialWraps))
-	BrawlAPI.ShowMessage(badMatCount + " material(s) wrap mode set to Clamp.", SCRIPT_NAME)
+	badMatCount = str(len(formattedBadMats))
+	BrawlAPI.ShowMessage(badMatCount + " material wrap mode settings changed to Clamp.", SCRIPT_NAME)
 
 main()
