c627d833233ecb4e40c5b1761b92a31696a32bb2
mawwwk <>
2024-06-08 22:45:35 -0500

    Delete Unused Stage Textures - Major code cleanup/rewrite

--- Plugins/PAC File Optimization/Delete Unused Stage Textures.py
+++ Plugins/PAC File Optimization/Delete Unused Stage Textures.py
@@ -1,175 +1,149 @@
 __author__ = "mawwwk"
-__version__ = "1.0.3"
+__version__ = "2.0"
 
 from BrawlCrate.API import *
 from BrawlLib.SSBB.ResourceNodes import *
 from BrawlCrate.UI import MainForm
 from BrawlLib.Internal import *
-from System.IO import *
 from mawwwkLib import *
 
-# Relatively safe to use, but WILL break Hanenbow based stages.
-## Begin global variables
-
 SCRIPT_NAME = "Locate Unused Stage Textures"
-texturesInMaterialsNamesList = []	# Names of textures used by mats
-texturesInPat0NamesList = []		# Names of textures used in PAT0s
-tex0List = []						# Tex0 nodes inside any brres
-deletedMatsNamesList = []			# Names of deleted materials (unused by any objects)
-deletedTex0NamesList = []			# Names of deleted tex0 nodes (unused by any material or pat0)
-cullAllMatsNamesList = []
-cullAllMDL0NamesList = []
-sizeCount = 0						# Sum of uncompressed bytes deleted
+# Relatively safe to use, but WILL break Hanenbow based stages.
 
-## End global variables
-## Begin helper methods
-
-# point parser to ModelData or TextureData behavior
-def checkBrres(node):
-	if "Model Data" in node.Name:
-		checkModelData(node)
-	elif "Texture Data" in node.Name:
-		checkTextureData(node)
-
-# Given a ModelData brres, loop through mdl0 and pat0 to populate used textures lists, then check for any tex0 nodes in the ModelData
-def checkModelData(brresNode):
-	modelsGroup = brresNode.FindChild("3DModels(NW4R)")
-	pat0Group = brresNode.FindChild("AnmTexPat(NW4R)")
-	modelDataTexturesGroup = brresNode.FindChild("Textures(NW4R)")
-	
-	# Loop through models to tally used textures, and remove unused mats
-	if modelsGroup:
-		for mdl0 in modelsGroup.Children:
-			texturesGroup = mdl0.FindChild("Textures")
-			modelMaterialsGroup = mdl0.FindChild("Materials")
-			# If the mdl0 has Texture references or Materials, dive in
-			if texturesGroup or modelMaterialsGroup:
-				checkMDL0(mdl0)
-	
-	# Loop through pat0s to tally the used textures
-	if pat0Group:
-		for pat0 in pat0Group.Children:
-			checkPAT0(pat0)
-	
-	# Check for tex0s in ModelData
-	# DON'T delete these -- in cases like Smashville, the references are loaded from the module or elsewhere outside the brres
-	if modelDataTexturesGroup:
-		for tex0 in modelDataTexturesGroup.Children:
-			# To simulate locality, delete the textures from "textures used" after logging them
-			if tex0.Name in texturesInMaterialsNamesList:
-				texturesInMaterialsNamesList.remove(tex0.Name)
-
-# Given a mdl0 node, delete any unused materials, then log any texture references
-def checkMDL0(mdl0):
-	global sizeCount
-	matsGroup = mdl0.FindChild("Materials")
-	mdl0TexturesGroup = mdl0.FindChild("Textures")
-	
-	# Check for materials not assigned to objects
-	if matsGroup:
-		
-		# Loop through materials in mdl0
-		for m in reverseResourceList(matsGroup.Children):
-		
-			# If not used in any objects, delete the material
-			if len(m._objects) == 0:
-				deletedMatsNamesList.append(m.Name)
-				sizeCount += m.UncompressedSize
-				m.Remove()
-			
-			# If material set to Cull All, don't delete it, but add to appropriate lists
-			elif "Cull_All" in str(m._cull):
-				cullAllMatsNamesList.append(m.Name)
-				cullAllMDL0NamesList.append(mdl0.Parent.Parent.Name + "/" + mdl0.Name)
-	
-	# In each model, get the "Textures" folder and populate the script's list of used textures
-	if mdl0TexturesGroup:
-	
-		# Loop through texture refs in mdl0
-		for texRef in reverseResourceList(mdl0TexturesGroup.Children):
-			if texRef.References:
-				texturesInMaterialsNamesList.append(texRef.Name)
-			
-			# If Texture node isn't used by any materials, delete it
-			else:
-				sizeCount += texRef.UncompressedSize
-				texRef.Remove()
-
-# Given a pat0 animation, loop through child entries and log textures used
-def checkPAT0(pat0):
-	for material in pat0.Children:
-		for texRef in material.Children:
-			# Loop through pat0 frames and log texture names
-			for frame in texRef.Children:
-				texturesInPat0NamesList.append(frame.Name)
-
-# Given a TextureData brres, log any tex0 nodes found, and delete any unused textures
-def checkTextureData(brresNode):
-	global tex0List
-	if not brresNode.HasChildren:
-		return
-	for tex0 in brresNode.FindChild("Textures(NW4R)").Children:
-		tex0List.append(tex0)
-
-## End helper methods
-## Start of main script
-
-# Confirmation prompt
 def main():
-	global tex0List
-	global sizeCount
-	msg = "Detect and remove unused materials and textures. \n\nTEX0s inside ModelData BRRES are ignored, and must be checked manually.\n\n"
-	msg += "NOTE: Always check the final results in-game, and save backups!\n\n"
-	msg += "Only recommended for use with Battlefield, FD, or Palutena-based stages. This WILL break Hanenbow-based stages and potentially stages with other bases."
-	PARENT_2_ARC = getParentArc()
+	bytesDeletedCount = 0		# Sum of uncompressed bytes worth of textures deleted
+	deletedMatsNamesList = []	# Names of deleted materials (unused by any objects)
+	deletedTex0NamesList = []	# Names of deleted tex0 nodes (unused by any material or pat0)
+	tex0NodeList = []
+	cullAllMatsNamesList = []
+	cullAllMDL0NamesList = []
+	texturesInMatNamesList = []
+	texturesInPat0NamesList = []
 	
-	# Get parent 2 ARC to verify the open file is a stage
-	if not (PARENT_2_ARC or BrawlAPI.ShowOKCancelPrompt(msg, SCRIPT_NAME)):
+	# Check for intended file structure
+	if not getParentArc():
+		msg = "2 ARC not found.\nThis script may still be run, but with unintended behavior. Continue?"
+		if not BrawlAPI.ShowYesNoDialog(msg, SCRIPT_NAME):
+			return
+	
+	# Confirmation dialog
+	startMsg = "Detect and remove unused materials and textures. \n\nTEX0s inside ModelData BRRES nodes are ignored, and must be checked manually.\n\n"
+	startMsg += "NOTE: Always check the final results in-game, and save backups!\n\n"
+	startMsg += "Only recommended for use with Battlefield, FD, or Palutena-based stages. This WILL break Hanenbow-based stages and potentially stages with other bases."
+	
+	if not BrawlAPI.ShowOKCancelPrompt(startMsg, SCRIPT_NAME):
 		return
 	
-	# Populate lists of used textures, and delete any unused materials
-	for brresNode in BrawlAPI.NodeListOfType[BRRESNode]():
-		checkBrres(brresNode)
+	brresNodeList = BrawlAPI.NodeListOfType[BRRESNode]()
+	
+	# Build list of tex0 nodes used in Texture Data brres
+	for brresNode in brresNodeList:
+		if "Texture Data" in brresNode.Name and brresNode.HasChildren:
+			textureGroup = brresNode.FindChild("Textures(NW4R)")
+			for tex0 in textureGroup.Children:
+				tex0NodeList.append(tex0)
+		
+	tex0NodeList = reverseResourceList(tex0NodeList)
+	
+	# Populate lists of used textures in Model Data, and also delete any unused materials
+	for brresNode in brresNodeList:
+		if "Model Data" in brresNode.Name:
+			
+			# Check 3DModels brres group
+			modelsGroup = brresNode.FindChild("3DModels(NW4R)")
+			if modelsGroup:
+				
+				# Loop through models
+				for mdl0 in modelsGroup.Children:
+					
+					# Check for materials unused by objects
+					matsGroup = mdl0.FindChild("Materials")
+					if matsGroup:
+						for mat in reverseResourceList(matsGroup.Children):
+						
+							# If not used in any objects, delete the material
+							if not len(mat._objects):
+								deletedMatsNamesList.append(mat.Name)
+								mat.Remove()
+							
+							# If material set to Cull All, don't delete it, but add to appropriate lists
+							elif "Cull_All" in str(mat._cull):
+								cullAllMatsNamesList.append(mat.Name)
+								cullAllMDL0NamesList.append(mdl0.Parent.Parent.Name + "/" + mdl0.Name)
+					
+					# In a model, get the Textures folder and populate the list of used textures
+					mdl0TexturesGroup = mdl0.FindChild("Textures")
+					if mdl0TexturesGroup:
+					
+						# Loop through texture refs in mdl0
+						for texRef in reverseResourceList(mdl0TexturesGroup.Children):
+							if texRef.References:
+								texturesInMatNamesList.append(texRef.Name)
+							
+							# If Texture node isn't used by any materials, delete it
+							else:
+								texRef.Remove()
+			
+			# Check PAT0 brres group
+			pat0Group = brresNode.FindChild("AnmTexPat(NW4R)")
+			if pat0Group:
+				for pat0 in pat0Group.Children:
+					for material in pat0.Children:
+						for texRef in material.Children:
+							# Loop through pat0 frames and append used texture names
+							for frame in texRef.Children:
+								texturesInPat0NamesList.append(frame.Name)
+			
+			# Check Textures brres group
+			modelDataTexturesGroup = brresNode.FindChild("Textures(NW4R)")
+			if modelDataTexturesGroup:
+				for tex0 in modelDataTexturesGroup.Children:
+					# Remove any found ModelData textures from being checked against the "global" TextureData list
+					if tex0.Name in texturesInMatNamesList:
+						texturesInMatNamesList.remove(tex0.Name)
 	
 	# If a texture in TextureData is not used by any model or pat0, delete it
-	tex0List = reverseResourceList(tex0List)
-	for tex0 in tex0List:
-		if not tex0.Name in (texturesInMaterialsNamesList + texturesInPat0NamesList):
+	for tex0 in tex0NodeList:
+		if not tex0.Name in (texturesInMatNamesList + texturesInPat0NamesList):
 			deletedTex0NamesList.append(tex0.Name)
-			sizeCount += tex0.UncompressedSize
+			bytesDeletedCount += tex0.UncompressedSize
 			tex0.Remove()
 	
-	# RESULTS dialog boxes
-	
+	# Results
 	matsDeletedCount = len(deletedMatsNamesList)
 	tex0DeletedCount = len(deletedTex0NamesList)
 	cullAllMatCount = len(cullAllMatsNamesList)
 	
 	# Print list of deleted tex0 names
-	if not matsDeletedCount and not tex0DeletedCount and not cullAllMatCount:
-		BrawlAPI.ShowMessage("No unused textures or materials detected", SCRIPT_NAME)
+	if not matsDeletedCount and not tex0DeletedCount:
+		message = "No unused textures or materials detected."
 	else:
 		message = ""
+		
+		# Append list of deleted mat names
 		if matsDeletedCount:
 			message += str(matsDeletedCount) + " materials deleted: \n\n"
-			message += listToString(deletedMatsNamesList, 10)
+			message += listToString(deletedMatsNamesList, 12)
 			message += "\n\n"
 		
+		# Append list of deleted texture names
 		if tex0DeletedCount:
 			message += str(tex0DeletedCount) + " textures deleted: \n\n"
-			message += listToString(deletedTex0NamesList, 10)
+			message += listToString(deletedTex0NamesList, 12)
 			message += "\n\n"
 		
+		# Include sum of bytes deleted
 		if message != "":
-			message += "\n\n" + str(sizeCount) + " bytes"
-			BrawlAPI.ShowMessage(message, "Deleted Unused Textures")
+			message += "\n" + str(bytesDeletedCount) + " bytes"
+	
+	BrawlAPI.ShowMessage(message, SCRIPT_NAME)
+	
+	# List any Cull All mats
+	if cullAllMatCount:
+		message = "Cull_All materials found:\n\n"
 		
-		# List any Cull_all mats
-		if cullAllMatCount > 0:
-			message = "Cull_All materials found:\n\n"
-			
-			for i in range(0, cullAllMatCount, 1):
-				message += "Model: " + str(cullAllMDL0NamesList[i]) + "\nMaterial: " + str(cullAllMatsNamesList[i]) + "\n\n"
-			BrawlAPI.ShowMessage(message, "Potential unused materials found")
+		for i in range(0, cullAllMatCount, 1):
+			message += "Model: " + str(cullAllMDL0NamesList[i]) + "\nMaterial: " + str(cullAllMatsNamesList[i]) + "\n\n"
+		BrawlAPI.ShowMessage(message, "Possible unused materials found")
 
 main()
