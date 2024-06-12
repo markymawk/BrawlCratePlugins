e1c739567efe6eabb4c9519d89b16d0a372bcf4d
mawwwk <>
2024-06-11 20:59:43 -0500

    Check UV Wrap Settings - No longer lists every texture in the file (old debugging thing)

--- Plugins/Check UV Wrap Settings.py
+++ Plugins/Check UV Wrap Settings.py
@@ -1,5 +1,5 @@
 __author__ = "mawwwk"
-__version__ = "1.0"
+__version__ = "1.1"
 
 from BrawlCrate.API import *
 from BrawlLib.SSBB.ResourceNodes import *
@@ -11,11 +11,11 @@ SCRIPT_NAME = "Check UV Wrap Settings"
 VALID_TEXTURE_RES = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
 
 def main():
-	startMsg = "Check if all materials using non-power-of-2 textures are correctly set to Clamp.\n" + \
+	START_MSG = "Check if all materials using non-power-of-2 textures are correctly set to Clamp.\n" + \
 	"Incorrect wrap settings for these materials will cause rendering issues on console.\n\n" + \
 	"Press OK to continue."
 	
-	if not BrawlAPI.ShowOKCancelPrompt(startMsg, SCRIPT_NAME):
+	if not BrawlAPI.ShowOKCancelPrompt(START_MSG, SCRIPT_NAME):
 		return
 	textureNamesToCheck_Width = []
 	textureNamesToCheck_Height = []
@@ -26,10 +26,10 @@ def main():
 			textureNamesToCheck_Width.append(tex0.Name)
 		if tex0.Height not in VALID_TEXTURE_RES:
 			textureNamesToCheck_Height.append(tex0.Name)
-
+	
 	# If all textures are powers of 2, quit
 	if not len(textureNamesToCheck_Width + textureNamesToCheck_Height):
-		BrawlAPI.ShowMessage("All textures in this file use powers of 2.", SCRIPT_NAME)
+		BrawlAPI.ShowMessage("All textures in this file use valid powers of 2.", SCRIPT_NAME)
 		return
 	
 	# Get list of materials used by these textures
@@ -43,47 +43,45 @@ def main():
 			continue
 		
 		for matRef in mat.Children:
-			badMat = False
+			isBadMat = False
 			
 			# Check Height / UWrapMode
-			for badTex in textureNamesToCheck_Width:
+			for textureName in textureNamesToCheck_Width:
 				
 				# If texture found and not set to Clamp, add to list
-				if badTex == matRef.Name and matRef.UWrapMode != MatWrapMode.Clamp:
+				if textureName == matRef.Name and matRef.UWrapMode != MatWrapMode.Clamp:
 					badMaterialWraps_U.append(matRef)
-					badMat = True
+					isBadMat = True
 				
 			# Check Width / VWrapMode
-			for badTex in textureNamesToCheck_Height:
+			for textureName in textureNamesToCheck_Height:
 				# If texture found and not set to Clamp, add to list
-				if badTex == matRef.Name and matRef.VWrapMode != MatWrapMode.Clamp:
+				if textureName == matRef.Name and matRef.VWrapMode != MatWrapMode.Clamp:
 					badMaterialWraps_V.append(matRef)
-					badMat = True
+					isBadMat = True
 			
-			if badMat:
+			if isBadMat:
 				badMaterialRefs.append(matRef)
 				
 	# If all materials are using Clamp as needed, quit
-	maxListItems = 15
+	MAX_LIST_ITEMS = 15
 	
 	if not len(badMaterialRefs):
-		msg = "No incorrect wrap mode settings found for the following textures:\n\n" + \
-		listToStringNoDuplicates(textureNamesToCheck_Height + textureNamesToCheck_Width, maxListItems)
-		
+		msg = "No incorrect wrap mode settings found."
 		BrawlAPI.ShowMessage(msg, SCRIPT_NAME)
 		return
 	
-	# If materials use incorrect wrap, prompt to auto set them
+	# Fill list with formatted material & matRef names
 	formattedBadMats = []
-	
 	for matRef in badMaterialRefs:
 		mat = matRef.Parent
 		mdl0 = mat.Parent.Parent
 		brres = mdl0.Parent.Parent
 		formattedBadMats.append(brres.Name + "/" + mdl0.Name + "/" + mat.Name + ": " + matRef.Name)
 	
+	# If materials use incorrect wrap, prompt to auto set them
 	msg = "The following material refs are not set to Clamp UVs:\n\n"
-	msg += listToString(formattedBadMats, maxListItems)
+	msg += listToString(formattedBadMats, MAX_LIST_ITEMS)
 	msg += "\nSet these to Clamp now?"
 	
 	# If not auto-setting any materials, quit
