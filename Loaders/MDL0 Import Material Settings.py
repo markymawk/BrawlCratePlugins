8826902415d4fb0f9148f0745e9da6dc22e1b120
mawwwk <>
2024-06-15 04:49:07 -0500

    Import MDL0 Material Settings - Add error check for selected model having more shaders than the imported one. Major code cleanup

--- Loaders/MDL0 Import Material Settings.py
+++ Loaders/MDL0 Import Material Settings.py
@@ -1,72 +1,76 @@
 __author__ = "mawwwk"
-__version__ = "1.3"
+__version__ = "1.4"
 
 from System.Windows.Forms import ToolStripMenuItem # Needed for all loaders
 from BrawlCrate.API import *
 from BrawlCrate.NodeWrappers import *
 from mawwwkLib import *
 
+SCRIPT_NAME = "Import Material Settings"
+
 ## Start enable check functions
 # Wrapper: MDL0Wrapper
 def EnableCheck(sender, event_args):
 	node = BrawlAPI.SelectedNode
-	matsGroup = getChildFromName(node, "Materials")
-	shadersGroup = getChildFromName(node, "Shaders")
-	sender.Enabled = (node is not None and node.HasChildren and matsGroup and shadersGroup)
+	sender.Enabled = node and node.FindChild("Materials") and node.FindChild("Shaders")
 
 ## End enable check functions
 ## Start loader functions
 
 def import_model_settings(sender, event_args):
-
-	# "Dest model" refers to the existing, selected mdl0 whose mats/shaders are being replaced
-	DEST_MODEL = BrawlAPI.SelectedNode
-	DEST_MODEL_WRAPPER = BrawlAPI.SelectedNodeWrapper
-	PARENT_MODELS_GROUP = BrawlAPI.SelectedNode.Parent
-	DEST_MODEL_MAT_GROUP = getChildFromName(DEST_MODEL, "Materials")
-	DEST_MODEL_SHADERS = getChildFromName(DEST_MODEL, "Shaders").Children
-	DEST_MODEL_OBJ_GROUP = getChildFromName(DEST_MODEL, "Objects")
 	
-	# Count number of models to later determine if one should be deleted
-	ORIGINAL_MODEL_COUNT = len(DEST_MODEL.Parent.Children)
+	# "Destination" model refers to the existing, selected mdl0 whose mats/shaders are being replaced
+	selNode = BrawlAPI.SelectedNode
+	selNodeWrapper = BrawlAPI.SelectedNodeWrapper
+	destination_MatGroup = selNode.FindChild("Materials") 
+	destination_Shaders = selNode.FindChild("Shaders")
+	destination_Objects = selNode.FindChild("Objects")
+	
+	# Count number of models pre-import to later ensure one was added
+	startingModelCount = len(selNode.Parent.Children)
 	
 	# Import temporary external MDL0
 	BrawlAPI.SelectedNodeWrapper.Parent.Parent.ImportModel()
 	
 	# "Source model" refers to the imported model from which settings are being imported
-	SOURCE_MODEL_INDEX = len(PARENT_MODELS_GROUP.Children)-1
-	SOURCE_MODEL = DEST_MODEL.Parent.Children[SOURCE_MODEL_INDEX]
-	SOURCE_MODEL_MAT_GROUP = getChildFromName(SOURCE_MODEL, "Materials")
-	SOURCE_MODEL_SHADERS = getChildFromName(SOURCE_MODEL, "Shaders").Children
-	SOURCE_MODEL_OBJ_GROUP = getChildFromName(SOURCE_MODEL, "Objects")
+	sourceModelIndex = len(BrawlAPI.SelectedNode.Parent.Children)-1
+	sourceModel = selNode.Parent.Children[sourceModelIndex]
+	source_MatGroup = sourceModel.FindChild("Materials")
+	source_Shaders = sourceModel.FindChild("Shaders")
+	source_Objects = sourceModel.FindChild("Objects")
 	
-	# If a model wasn't imported (i.e. window closed), terminate the script
-	if (ORIGINAL_MODEL_COUNT == len(DEST_MODEL.Parent.Children)):
+	# If number of models is unchanged (i.e. window closed), terminate the script
+	if (startingModelCount == len(selNode.Parent.Children)):
 		return
 	
 	# Start shader import
-	# If dest model has fewer shaders than source model, add more to dest model until the amount of shaders in each mdl0 is equal
-	DEST_SHADER_COUNT = len(DEST_MODEL_SHADERS)
-	SOURCE_SHADER_COUNT = len(SOURCE_MODEL_SHADERS)
+	dest_ShaderCount = len(destination_Shaders.Children)
+	source_ShaderCount = len(source_Shaders.Children)
 	
-	while DEST_SHADER_COUNT < SOURCE_SHADER_COUNT:
-		DEST_MODEL_WRAPPER.NewShader()
-		DEST_SHADER_COUNT += 1
-		
+	# If destination model has more shaders than the imported model, don't add more (mostly an error check, this usually shouldn't happen)
+	if source_ShaderCount < dest_ShaderCount:
+		BrawlAPI.ShowWarning("Source model has more shaders than selected model.\nSome shaders may be unaffected.", SCRIPT_NAME)
+		dest_ShaderCount = source_ShaderCount
+	# If dest model has fewer shaders than source model, add shaders to the destination model until the amount of shaders is equal between each
+	else:
+		while dest_ShaderCount < source_ShaderCount:
+			selNodeWrapper.NewShader()
+			dest_ShaderCount += 1
+	
 	# For each shader in dest group, replace it with corresponding source shader
-	for i in range(0, DEST_SHADER_COUNT, 1):
-		sourceShader = SOURCE_MODEL_SHADERS[i]
-		destShader = DEST_MODEL_SHADERS[i]
+	for i in range(dest_ShaderCount):
+		sourceShader = source_Shaders.Children[i]
+		destShader = destination_Shaders.Children[i]
 		
 		destShader.Replace(sourceShader)
 	
 	# End shader import
 	# Begin material import
-	sourceMatsList = getChildNodes(SOURCE_MODEL_MAT_GROUP)
+	sourceMatsList = getChildNodes(source_MatGroup)
 	
 	# Replace materials that share names
-	for destMat in DEST_MODEL_MAT_GROUP.Children:
-		sourceMat = getChildFromName(SOURCE_MODEL_MAT_GROUP, destMat.Name, True)
+	for destMat in destination_MatGroup.Children:
+		sourceMat = source_MatGroup.FindChild(destMat.Name)
 		
 		# If material exists in source model, assign it to dest model
 		if sourceMat:
@@ -76,36 +80,37 @@ def import_model_settings(sender, event_args):
 	
 	# Copy any remaining materials (those that don't share names)
 	for sourceMat in sourceMatsList:
-		newMat = DEST_MODEL_WRAPPER.NewMaterial()
+		newMat = selNodeWrapper.NewMaterial()
 		newMat.Replace(sourceMat)
 		newMat.Name = sourceMat.Name
 	
 	# End material import
-	# Begin object transparency settings
-	for destObj in DEST_MODEL_OBJ_GROUP.Children:
-		sourceObj = getChildFromName(SOURCE_MODEL_OBJ_GROUP, destObj.Name)
+	# Begin object DrawPass setings (transparency, etc.)
+	for destObj in destination_Objects.Children:
+		sourceObj = source_Objects.FindChild(destObj.Name)
+		
 		# Copy draw pass settings (XLU/OBJ) to each object
 		if sourceObj and destObj._drawCalls:
 			destObj._drawCalls[0].DrawPass = sourceObj._drawCalls[0].DrawPass
 	
 	# Delete temp mdl0 
-	removeNode(SOURCE_MODEL)
+	removeNode(sourceModel)
 	
-	# Select destination mdl0 node again
-	DEST_MODEL.Parent.SelectChildAtIndex(DEST_MODEL.Index)
+	# Select destination mdl0 node again. (This may seem redundant, but Index is preserved from before)
+	selNode.Parent.SelectChildAtIndex(selNode.Index)
 	
-	# Number of materials imported that aren't assigned to any objects -- may be incorrectly assigned or unused
+	# Number of materials imported that are not assigned to any objects
 	EMPTY_MATS_COUNT = len(sourceMatsList)
 	
-	# Force rebuild node to correctly update model
-	DEST_MODEL.Rebuild(True)
+	# I don't know what this does but it was requested
+	selNode.Rebuild(True)
 	
 	message = "Materials and shaders imported successfully!"
 	
 	if EMPTY_MATS_COUNT:
 		message += "\n\n" + str(EMPTY_MATS_COUNT) + " materials unassigned to objects (may be incorrectly assigned, or unused)"
 		
-	BrawlAPI.ShowMessage(message, "Success")
+	BrawlAPI.ShowMessage(message, SCRIPT_NAME)
 
 ## End loader functions
 ## Start context menu add
