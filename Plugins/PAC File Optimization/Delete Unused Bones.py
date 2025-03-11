3b631a721feab59912463bb30e3b3a610cb0192f
mawwwk <>
2025-03-11 04:38:31 -0500

    Delete Unused Bones - Clearer checks for collision bones. Other cleanup

--- Plugins/PAC File Optimization/Delete Unused Bones.py
+++ Plugins/PAC File Optimization/Delete Unused Bones.py
@@ -1,7 +1,6 @@
 __author__ = "mawwwk"
-__version__ = "1.2"
+__version__ = "1.3"
 
-from BrawlCrate.UI import MainForm
 from mawwwkLib import *
 
 ## Begin global variables
@@ -24,7 +23,7 @@ def isSingleBindModel(model):
 	return True
 	
 # Recursively look at bone's children to determine if it's used by any objects or collisions
-def checkBone(bone, safeBones):
+def checkBone(bone, collisionBoneNames):
 	
 	# Count bones removed in this model
 	bonesRemovedCount = 0
@@ -34,13 +33,12 @@ def checkBone(bone, safeBones):
 		for childBone in getChildNodes(bone):
 		
 			# Increment count if bone was removed
-			bonesRemovedCount += checkBone(childBone, safeBones)
+			bonesRemovedCount += checkBone(childBone, collisionBoneNames)
 	
 	# If bone isn't the last bone, has no children, and isn't used by any objects, delete the bone
 	isOnlyBone = isinstance(bone.Parent, MDL0GroupNode) and len(bone.Parent.Children) == 1 and not bone.HasChildren
-	hasGeometry = "HasGeometry" in str(bone._boneFlags)
 	
-	if not isOnlyBone and not (bone.HasChildren or len(bone.VisibilityDrawCalls) or hasGeometry or bone in safeBones):
+	if not isOnlyBone and (not bone.Name in collisionBoneNames) and not (bone.HasChildren or len(bone.VisibilityDrawCalls) or len(bone.SingleBindObjects)):
 		removeNode(bone)
 		bonesRemovedCount += 1
 	
@@ -51,9 +49,9 @@ def checkBone(bone, safeBones):
 
 def main():
 	bonesDeletedCount = 0
-	safeBones = [] # Collision bones, etc
+	collisionBoneNames = []
 	modelsModifiedList = []
-	nonSingleBindModels = [] # Models that use non-SingleBind objects, and thus are skipped
+	nonSingleBindModels = [] # Models that use non-SingleBind objects, skipped and listed at end
 	
 	if not BrawlAPI.RootNode or not BrawlAPI.RootNode.FindChild("2"):
 		BrawlAPI.ShowError("2 ARC not found.", SCRIPT_NAME)
@@ -68,19 +66,21 @@ def main():
 	
 	# Get list of bone nodes used by collisions
 	for obj in BrawlAPI.NodeListOfType[CollisionObject]():
-		safeBones.append(obj.LinkedBone)
+		linkedBone = obj._boneName
+		if linkedBone != "":
+			collisionBoneNames.append(linkedBone)
 	
 	# Loop through all MDL0s in file
 	for mdl0 in BrawlAPI.NodeListOfType[MDL0Node]():
 	
 		# If model name matches any certain "safe" model, don't delete any bones
-		isSafeModel = False
+		isModelNameSafe = False
 		for safeName in MODEL_NAMES_TO_AVOID:
-			if safeName in mdl0.Name.lower():
-				isSafeModel = True
+			isModelNameSafe = isModelNameSafe or safeName in mdl0.Name.lower()
+		
 		modelBonesGroup = mdl0.FindChild("Bones")
 		
-		if isSafeModel or mdl0.IsStagePosition or not modelBonesGroup:
+		if isModelNameSafe or mdl0.IsStagePosition or not modelBonesGroup:
 			continue
 		
 		# If parent model has any non-SingleBind objects, assume there is complex rigging and ignore further child bones, to be safe.
@@ -93,17 +93,17 @@ def main():
 		
 		# Start looping through bones in the mdl0, and delete any unused bones
 		for bone in modelBonesList:
-			modelBonesDeletedCount = checkBone(bone, safeBones)
+			modelBonesDeletedCount = checkBone(bone, collisionBoneNames)
+			bonesDeletedCount += modelBonesDeletedCount
 			
 			# If any bones deleted, append the BRRES name and MDL0 name to modelsModifiedList[]
 			if modelBonesDeletedCount:
 				modelsModifiedList.append(mdl0.Parent.Parent.Name + "/" + mdl0.Name)
-				bonesDeletedCount += modelBonesDeletedCount
 	
 	# Results
 	# If any edits made, list models modified
 	if len(modelsModifiedList):
-		resultsMsg = str(bonesDeletedCount) + " unused bones found and deleted in:\n" + listToString(modelsModifiedList)
+		resultsMsg = str(bonesDeletedCount) + " unused bone(s) found and deleted in:\n" + listToString(modelsModifiedList, 25)
 	
 	# If no edits made
 	else:
@@ -112,6 +112,7 @@ def main():
 	# If any non-SingleBind models were skipped, list them for clarity
 	if len(nonSingleBindModels):
 		resultsMsg += "\n\nSkipped " + str(len(nonSingleBindModels)) + " models from having non-SingleBind objects:\n" + listToString(nonSingleBindModels)
+	
 	BrawlAPI.ShowMessage(resultsMsg, SCRIPT_NAME)
 	
 main()
